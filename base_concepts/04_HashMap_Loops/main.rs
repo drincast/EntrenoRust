f973acfe0219bee030617f7e51a522a7e978457e
drincast <drincast@gmail.com>
2022-10-31 20:43:05 -0500

    Se agrega ejemplo de tipo de coleccion de datos HasMap

---
+++ base_concepts/04_HashMap_Loops/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+fn main(){
+    let mut hash_map: HashMap<String, String> = HashMap::new();
+
+    hash_map.insert("key1".to_string(), "value1".to_string());
+    hash_map.insert(String::from("key2"), String::from("value2"));
+    hash_map.insert(String::from("key3"), String::from("value3"));
+
+    println!("key1: {:?}, key2: {:?}, key3: {:?}", hash_map.get("key1"), hash_map.get("key2")
+        , hash_map.get("key3"));
+
+    hash_map.remove("key3");
+
+    println!("key1: {:?}, key2: {:?}, key3: {:?}", hash_map.get("key1"), hash_map.get("key2")
+        , hash_map.get("key3"));
+}
