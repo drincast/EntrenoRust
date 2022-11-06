c0201eceda7ba0be6180b8896151df5764655ebc
drincast <drincast@gmail.com>
2022-11-06 08:24:54 -0500

    Se agrega ejemplo ciclo while

--- base_concepts/04_HashMap_Loops/main.rs
+++ base_concepts/04_HashMap_Loops/main.rs
@@ -1,5 +1,7 @@
 //se debe llamar la definicion de HasMap
 use std::collections::HashMap;
+use std::convert::TryInto;
+// use hashbrown::HashMap;
 
 fn main(){
     //definicion de HasMap
@@ -17,7 +19,8 @@ fn main(){
     println!("key1: {:?}, key2: {:?}, key3: {:?}", hash_map.get("key1"), hash_map.get("key2")
         , hash_map.get("key3"));
 
-        println!("--------------------------------------------------------------\n");
+    println!("--------------------------------------------------------------\n");
+    println!("LOOP");
 
     let mut id_key : i8 = 1;
     let str_key : String = String::from("key");
@@ -28,7 +31,22 @@ fn main(){
 
         let index = format!("{}{}", str_key, &id_key.to_string());
 
-        println!("{}: {:?}", index, hash_map.get(&index));
+        println!("{}: {:?}", index, &hash_map.get(&index));
         id_key = id_key + 1;
     }
+
+    println!("--------------------------------------------------------------\n");
+    println!("WHILE");
+
+    let number_items = hash_map.keys().len();
+    id_key = 0;
+    println!("number_items: {}", number_items);
+    while id_key < number_items.try_into().unwrap() {
+        id_key = id_key + 1;
+        let index = format!("{}{}", str_key, &id_key.to_string());
+        println!("{}: {:?}", index, &hash_map.get(&index));
+    }
+
+    println!("--------------------------------------------------------------\n");
+    println!("FOR");
 }
