047b1e3426837d6030f46ee611768d09c7c0e51e
drincastX <drincast@gmail.com>
2022-12-27 21:27:11 -0500

    se agrega seccion de control de errores 05_error_control

---
+++ base_concepts/05_error_control/main.rs
@@ -0,0 +1,51 @@
+// #[derive(Debug, Copy, Clone)]
+struct Person {
+    first: String,
+    middle: Option<String>,
+    last: String,
+}
+
+fn build_full_name(person: &Person) -> String {
+    let mut full_name = String::new();
+    full_name.push_str(&person.first);    
+
+    // TODO: Implement the part of this function that handles the person's middle name.
+    match &person.middle {
+        Some(a) => {
+            full_name.push_str(" ");
+            full_name.push_str(&a);
+            full_name.push_str(" ");
+        },
+        None => full_name.push_str(" "),
+    }
+
+    full_name.push_str(&person.last);
+    println!("{}", &full_name);
+    full_name
+}
+
+fn main() {
+    let john = Person {
+        first: String::from("James"),
+        middle: Some(String::from("Oliver")),
+        last: String::from("Smith"),
+    };
+    assert_eq!(build_full_name(&john), "James Oliver Smith");
+
+    let alice = Person {
+        first: String::from("Alice"),
+        middle: None,
+        last: String::from("Stevens"),
+    };
+    assert_eq!(build_full_name(&alice), "Alice Stevens");
+
+    let bob = Person {
+        first: String::from("Robert"),
+        middle: Some(String::from("Murdock")),
+        last: String::from("Jones"),
+    };
+    assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+
+    build_full_name(&john);
+    println!("fin");
+}
